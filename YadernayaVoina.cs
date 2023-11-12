cf81d54e0d9eac3b4940c92291e3e9ab6a31cf16
Mamkachap <150594183+Mamkachap@users.noreply.github.com>
2023-11-12 15:31:18 +0300

    Add files via upload

---
+++ YadernayaVoina.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // ”ничтожить €дерную войну
+    void Start()
+    {
+        Destroy(gameObject);
+        print("Destroy.cs.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
